255ae1b0ec919feb79d5ad48d37fd2ea92c8a882
Dominic Frey <frey.dominic@outlook.com>
2021-06-03 12:25:06 +0200

    fix: add global prefix to route

--- src/thesis/thesis.controller.ts
+++ src/thesis/thesis.controller.ts
@@ -1,46 +1,32 @@
-import { Controller, Get, Post } from "@nestjs/common";
-import { ThesisService } from "./thesis.service";
+import { Controller, Get, Post } from '@nestjs/common';
+import { ThesisService } from './thesis.service';
 
-@Controller('api')
+@Controller()
 export class ThesisController {
-
   constructor(private thesisService: ThesisService) {}
 
   // Get all theses'
   @Get('theses')
-  async getTheses() {
-
-  }
+  async getTheses() {}
 
   // Get one thesis
   @Get('thesis/detail/:thesisId')
-  async getThesis() {
-
-  }
+  async getThesis() {}
 
   // Add new thesis
   @Post('thesis')
-  async addThesis() {
-
-  }
+  async addThesis() {}
 
   // Edit thesis
   // student can select the thesis and supervisor can change the informations in the thesis
   @Post('thesis/:thesisId')
-  async updateThesis() {
-
-  }
+  async updateThesis() {}
 
   // Student select one thesis
   @Post('thesis/detail/:userId')
-  async selectThesis() {
-
-  }
-
+  async selectThesis() {}
 
   // Get all these of one supervisor
   @Get('thesis/:userId')
-  async getSupervisorThese() {
-
-  }
+  async getSupervisorThese() {}
 }
